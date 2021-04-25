862c12e93762df4d09060db8c401a51e5a377301
Rupali Hirave <rupali.hirave1520@gmail.com>
2021-04-25 18:50:08 +0530

    modifiedtestcas

--- src/com/tescases/InputForm_SimpleFormDemo.java
+++ src/com/tescases/InputForm_SimpleFormDemo.java
@@ -1,8 +1,16 @@
 package com.tescases;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class InputForm_SimpleFormDemo {
 
 	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\Automation softwares\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
+		
 		
 
 	}
