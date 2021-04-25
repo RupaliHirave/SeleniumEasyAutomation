db1ee4b88cb9a6803c498dc9c0843977b03755f6
Rupali Hirave <rupali.hirave1520@gmail.com>
2021-04-25 20:02:07 +0530

    added code for Simple form demo

--- src/com/tescases/InputForm_SimpleFormDemo.java
+++ src/com/tescases/InputForm_SimpleFormDemo.java
@@ -1,15 +1,49 @@
 package com.tescases;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class InputForm_SimpleFormDemo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
-		System.setProperty("webdriver.chrome.driver", "D:\\Automation softwares\\chromedriver.exe");
+		//Launching browser
+		System.setProperty("webdriver.chrome.driver", "D:\\Automation softwares\\drivers\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
-		driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.get("https://www.seleniumeasy.com/test/");
+		
+		//handling popup
+		WebDriverWait wait=new WebDriverWait(driver, 15);
+		WebElement closepopup=driver.findElement(By.cssSelector("#at-cv-lightbox-header #at-cv-lightbox-close"));
+		wait.until(ExpectedConditions.visibilityOf(closepopup)).click();
+		
+		//Scroll to start practice button
+		JavascriptExecutor js=(JavascriptExecutor)driver;
+		WebElement startpractice=driver.findElement(By.cssSelector("#btn_basic_example"));
+		js.executeScript("arguments[0].scrollIntoView();", startpractice);
+		//click on start practice button
+		startpractice.click();
+		Thread.sleep(2000);
+		//click on simple demo form
+		driver.findElement(By.cssSelector("div.list-group a[href=\"./basic-first-form-demo.html\"]")).click();
+		Thread.sleep(2000);
+		//Enter massege
+		driver.findElement(By.cssSelector("#user-message.form-control")).sendKeys("My name is Rupali");
+		Thread.sleep(2000);
+		driver.findElement(By.cssSelector("button[class='btn btn-default']")).click();
+		
+		System.out.println("Testcase is passed");
+		
+		driver.close();
+		
+		
 		
 		
 
